e732adf0bc2bb2ef5c81c2ea57b1993ec949317c
AlexandruPiciorus <alexandru.piciorus@nexttech.ro>
2024-03-15 15:19:29 +0200

    first commit

---
+++ src/main/java/com/example/codemaster/CodemasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.codemaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodemasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodemasterApplication.class, args);
+    }
+
+}
